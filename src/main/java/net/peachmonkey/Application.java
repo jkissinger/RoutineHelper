aa91cfb35c89f7acf23e3c4fbc21454bb74bb875
Jonathan Kissinger <kissi1j@cmich.edu>
2017-01-22 13:11:03 -0500

    Code cleanup.

--- src/main/java/net/peachmonkey/Application.java
+++ src/main/java/net/peachmonkey/Application.java
@@ -1,14 +1,10 @@
 package net.peachmonkey;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
-	private static final Logger LOGGER = LogManager.getLogger();
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
